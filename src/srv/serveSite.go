43d53825116cf4842f0cb9a425c257e1c566e30d
H3rmt <stemmer.enrico@gmail.com>
2022-09-11 18:14:14 +0200

    added url sanitation

--- src/srv/serveSite.go
+++ src/srv/serveSite.go
@@ -3,6 +3,7 @@ package srv
 import (
 	"errors"
 	"fmt"
+	"html"
 	"net/http"
 	"strings"
 	"time"
@@ -21,32 +22,35 @@ func getSite(request *http.Request, availableEncodings *map[Encoding]bool) (*[]b
 		return data, "", code, "text/html", errors.New(fmt.Sprintf("URI to long (%v)", len(request.URL.String())))
 	}
 
+	url := html.EscapeString(request.URL.Path)
+	host := html.EscapeString(request.Host)
+
 	for _, forbidden := range settings.GetSettings().Forbidden.Get() {
 		switch forbidden.Type {
 		case settings.FileExtension:
-			if strings.HasSuffix(request.URL.Path, "."+forbidden.Data) {
-				data, code := GetErrorSite(http.StatusForbidden, request.Host, request.URL.Path, "forbidden by FileExtension")
-				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by extension: %s (%s)", forbidden.Data, request.URL.Path))
+			if strings.HasSuffix(url, "."+forbidden.Data) {
+				data, code := GetErrorSite(http.StatusForbidden, host, url, "forbidden by FileExtension")
+				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by extension: %s (%s)", forbidden.Data, url))
 			}
 		case settings.AbsoluteFile:
-			if request.URL.Path == forbidden.Data {
-				data, code := GetErrorSite(http.StatusForbidden, request.Host, request.URL.Path, "forbidden by absolute path")
+			if url == forbidden.Data {
+				data, code := GetErrorSite(http.StatusForbidden, host, url, "forbidden by absolute path")
 				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by absolute path: %s", forbidden.Data))
 			}
 		case settings.AbsoluteDirectory:
-			if strings.HasPrefix(request.URL.Path, forbidden.Data) {
-				data, code := GetErrorSite(http.StatusForbidden, request.Host, request.URL.Path, "forbidden by absolute DirPath")
-				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by absolute DirPath: %s (%s)", forbidden.Data, request.URL.Path))
+			if strings.HasPrefix(url, forbidden.Data) {
+				data, code := GetErrorSite(http.StatusForbidden, host, url, "forbidden by absolute DirPath")
+				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by absolute DirPath: %s (%s)", forbidden.Data, url))
 			}
 		case settings.Regex:
-			if forbidden.Regex.Match([]byte(request.URL.Path)) {
-				data, code := GetErrorSite(http.StatusForbidden, request.Host, request.URL.Path, "forbidden by regex")
-				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by regex: %s (%s)", forbidden.Data, request.URL.Path))
+			if forbidden.Regex.Match([]byte(url)) {
+				data, code := GetErrorSite(http.StatusForbidden, host, url, "forbidden by regex")
+				return data, "", code, "text/html", errors.New(fmt.Sprintf("forbidden by regex: %s (%s)", forbidden.Data, url))
 			}
 		}
 	}
 
-	pathSplit := strings.Split(request.URL.Path, "/")[1:]
+	pathSplit := strings.Split(url, "/")[1:]
 
 	depth := len(pathSplit)
 
@@ -60,10 +64,10 @@ func getSite(request *http.Request, availableEncodings *map[Encoding]bool) (*[]b
 	file, ok := dir.files[pathSplit[depth-1]]
 	if !ok {
 		if _, ok := dir.dirs[pathSplit[depth-1]]; ok {
-			data, code := GetErrorSite(http.StatusNotFound, request.Host, request.URL.Path, fmt.Sprintf("%s is no file, but a directory", pathSplit[depth-1]))
+			data, code := GetErrorSite(http.StatusNotFound, host, url, fmt.Sprintf("%s is no file, but a directory", pathSplit[depth-1]))
 			return data, "", code, "text/html", errors.New(fmt.Sprintf("no site data for: %v", pathSplit))
 		}
-		data, code := GetErrorSite(http.StatusNotFound, request.Host, request.URL.Path, "")
+		data, code := GetErrorSite(http.StatusNotFound, host, url, "")
 		return data, "", code, "text/html", errors.New(fmt.Sprintf("no site data for: %s", pathSplit))
 	}
 	data, encoding := file.data.getSmallest(availableEncodings)
